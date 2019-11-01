15db3abe89f90445d8b76450ba32eff853ebe7af
Buddhika Lakshan <buddhika.lakshan123@gmail.com>
2019-11-01 14:40:15 +0530

    test

---
+++ testsub/s.sql
@@ -0,0 +1,25 @@
+select                 
+                        round((round(
+                                sum((subtotal-(subtotal*commission/100)-(booking_fares.admin_charge))),2) + 
+                                sum(round( ifnull(booking_fares.far_pickup_cutoff_allowance,0),2))+
+                                sum(round( ifnull(booking_fares.cancel_charge,0),2))+
+                                sum(round( ifnull(booking_fares.tip,0),2))+
+                                sum(round( ifnull(booking_fares.pickup_charge_per_km,0),2))+
+                                sum(round( ifnull(booking_fares.trip_allowance,0),2))+
+                                ),2)as driver_total_income ,
+                
+                
+                        from booking_fares 
+                        where driverid  = '55' AND DATE (created) = CURDATE();
+
+
+
+
+                        select  (
+SUM(booking_fares.subtotal-(booking_fares.subtotal*commission/100)-(booking_fares.admin_charge))+
+SUM(booking_fares.far_pickup_cutoff_allowance)+
+SUM(booking_fares.far_pickup_cutoff_allowance)+
+SUM(booking_fares.cancel_charge)+
+SUM(booking_fares.tip)+
+SUM(booking_fares.pickup_charge_per_km)+
+SUM(booking_fares.trip_allowance))AS ddd FROM  booking_fares  where driverid  = 50 AND DATE (created) = CURDATE();
